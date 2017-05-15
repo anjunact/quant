e7f411101b4fbb67832c18581985c8e07de4f2d2
anjuncc <anjuncc@163.com>
2017-05-15 20:50:02 +0800

    web

--- web/quant/views.py
+++ web/quant/views.py
@@ -1,5 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from django.template import loader
 def index(request):
-    return HttpResponse("hello world")
+    # return HttpResponse("hello world")
+    template = loader.get_template('quant/index.html')
+    context ={
+        "a":23
+    }
+    return HttpResponse(template.render(context,request))
